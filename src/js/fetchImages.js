87d5dae3d4649a5d1966c37e4d47c25fe00b213f
derofeeva2023 <109346620+derofeeva2023@users.noreply.github.com>
2023-01-11 13:34:05 +0200

    key

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,6 @@
 export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
-    `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+    `https://pixabay.com/api/?key=32769919-2e14edf0f9a5b6dead7f3ed62&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
     .then(async response => {
       if (!response.ok) {
